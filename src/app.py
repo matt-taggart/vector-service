4dde2f940783802f6701a7f48277adff8502198c
Matt Taggart <mtaggart89@gmail.com>
2023-07-03 11:49:06 -0700

    modify jwt encoding and recursively process documents

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import time
 from requests.auth import HTTPBasicAuth
 import json
 from html2text import html2text
@@ -8,9 +9,15 @@ import textwrap
 import openai
 import markdown
 import jwt
+from hashlib import sha256
+import urllib.parse
+from urllib.parse import urlparse, parse_qs, quote
+import marshal
 from sqlalchemy import create_engine, text, select, delete, Table, MetaData
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from supabase import create_client, Client
+import atlassian_jwt_auth 
 
 import vecs
 from llama_index import ListIndex, SimpleWebPageReader, SimpleDirectoryReader, Document, StorageContext, load_index_from_storage
@@ -39,6 +46,11 @@ PROJECT_PILOT_API_KEY = os.environ.get("PROJECT_PILOT_API_KEY")
 app = Flask(__name__)
 parser = SimpleNodeParser()
 
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.environ.get("SUPABASE_ANON_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
 jobstores = {
     'default': SQLAlchemyJobStore(DB_CONNECTION_STRING)
 }
@@ -53,22 +65,60 @@ scheduler = BackgroundScheduler(daemon=True, jobstores=jobstores)
 scheduler.add_job(job, trigger='cron', hour=21, minute=43)
 scheduler.start()
 
-@app.route('/initialize', methods=['POST'])
-@authenticate
-def create_embeddings():
-    data = request.json
-    client_key = data['clientKey']
-    base_url = data['baseUrl']
-    org_name = base_url.split('https://')[1].split('.')[0]
+def create_query_string_hash(method, url):
+    # Parse the URL
+    parsed_url = urlparse(url)
 
-    url = f"{base_url}/api/v2/pages?body-format=storage"
+    # Extract and URL-encode the path
+    path = quote(parsed_url.path)
 
-    token_header = request.headers.get('Authorization')
-    token = token_header.split(' ')[1]
+    # Extract and URL-encode the query parameters
+    query_params = parse_qs(parsed_url.query)
+    canonical_query_string = '&'.join([
+        f'{quote(param)}={quote(value[0])}'
+        for param, value in sorted(query_params.items())
+    ])
+
+    # Create the canonical request string
+    canonical_request = f'{method}&{path}&{canonical_query_string}'
+
+    # Compute the SHA-256 hash of the canonical request string
+    return sha256(canonical_request.encode('utf-8')).hexdigest()
+
+def generate_atlassian_jwt(client_info_key, client_key, method, url, shared_secret):
+    # Generate the QSH
+    qsh = create_query_string_hash(method, url)
+
+    # Get the current time
+    now = int(time.time())
+
+    # Create the JWT payload
+    payload = {
+        'iss': client_info_key,
+        'iat': now,
+        'exp': now + 180,
+        'qsh': qsh,
+        'aud': client_key,
+    }
+
+    # Encode the JWT
+    encoded_jwt = jwt.encode(payload, shared_secret, algorithm='HS256')
+
+    return encoded_jwt
+
+
+def fetchPages(org_name, fragment, client_key, shared_secret, key, documents = []):
+    base_url = f"https://{org_name}.atlassian.net"
+    url = f"{base_url}{fragment}"
+    method = 'GET'
+    index = url.find("/api")
+    formatted_fragment = url[index:]
+
+    encoded_jwt = generate_atlassian_jwt(key, client_key, method, formatted_fragment, shared_secret)
 
     headers = {
       "Accept": "application/json",
-      "Authorization": f"JWT {token}"
+      "Authorization": f"JWT {encoded_jwt}"
     }
 
     payload = json.dumps({
@@ -77,7 +127,8 @@ def create_embeddings():
       "title": "<string>",
       "parentId": "<string>",
       "_links": {
-          "webui": "<string>"
+          "webui": "<string>",
+          "links": "<string>",
       },
       "body": {
         "representation": "storage",
@@ -93,9 +144,9 @@ def create_embeddings():
     )
 
     pages = json.loads(response.text)
-    pages_results = pages['results']
 
-    documents = []
+    next_link = pages.get("_links", {}).get("next", None)
+    pages_results = pages['results']
 
     for page in pages_results:
         id = page['id']
@@ -115,6 +166,26 @@ def create_embeddings():
             doc_id=id,
             extra_info={'title': title, 'url': full_url }
         ))
+
+    if next_link:
+        return fetchPages(org_name, next_link, client_key, shared_secret, key, documents)
+
+    return documents
+
+
+@app.route('/initialize', methods=['POST'])
+@authenticate
+def create_embeddings():
+    data = request.json
+    key = data['key']
+    client_key = data['clientKey']
+    base_url = data['baseUrl']
+    org_name = base_url.split('https://')[1].split('.')[0]
+
+    response = supabase.table('companies').select('shared_secret').eq('client_key', client_key).execute()
+    shared_secret = response.data[0]['shared_secret']
+
+    documents = fetchPages(org_name, "/wiki/api/v2/pages?body-format=storage&limit=2", client_key, shared_secret, key)
 
     engine = create_engine(DB_CONNECTION_STRING)
 
